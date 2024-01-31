108124231dd7d00f2442bac1dcfca5f52929647d
shohrux4045 <zokirovshohrux2020@gmail.com>
2024-01-31 15:42:16 +0500

    git commit -m "Initial commit"

--- todo/models.py
+++ todo/models.py
@@ -10,9 +10,3 @@ class Todo(models.Model):
     def __str__(self):
         return self.title
 
-
-class Category(models.Model):
-    title = models.CharField(max_length=150)
-
-    def __str__(self):
-        return self.title
